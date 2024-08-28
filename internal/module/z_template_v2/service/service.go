42c1c2771b7bddd8105002386b2b6cd54cdd7004
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-08-28 15:17:05 +0700

    initial codebase project boilerplate fiber

---
+++ internal/module/z_template_v2/service/service.go
@@ -0,0 +1,15 @@
+package service
+
+import "go-product-service/internal/module/z_template_v2/ports"
+
+var _ ports.XxxService = &xxxService{}
+
+type xxxService struct {
+	repo ports.XxxRepository
+}
+
+func NewXxxService(repo ports.XxxRepository) *xxxService {
+	return &xxxService{
+		repo: repo,
+	}
+}
